d5bcc04c5f5d8ed07e80d450e4ec5c04bbe3c33f
dawg <dawg@dawgfoto.de>
2011-11-02 21:49:26 +0100

    cleanup bitmap

--- src/guip/bitmap.d
+++ src/guip/bitmap.d
@@ -1,241 +1,228 @@
 module guip.bitmap;
 
-import std.conv, std.exception, std.string, std.range : outputRangeObject;
-
-import guip.color;
-import guip.rect;
-import guip.size;
-
-import freeimage.freeimage;
 import core.atomic;
+import std.conv, std.exception, std.string, std.range;
+import guip.color, guip.rect, guip.size;
+import freeimage.freeimage;
 
-//debug=PRINTF;
-debug(PRINTF) import std.stdio : writeln, printf;
-
-////////////////////////////////////////////////////////////////////////////////
-//
-////////////////////////////////////////////////////////////////////////////////
-
-/**
-   stub ColorTable
-*/
-class ColorTable
-{
-  enum
-  {
-    kColorsAreOpaque_Flag,
-  }
-  ubyte flags;
-}
-
-/**
-   Bitmap
+/*
+ * Bitmap
  */
-struct Bitmap {
-  enum Config {
-    NoConfig,   //!< bitmap has not been configured
-    A1,         //!< 1-bit per pixel, (0 is transparent, 1 is opaque)
-    A8,         //!< 8-bits per pixel, with only alpha specified (0 is
-              //!< transparent, 0xFF is opaque)
-    Index8,     //!< 8-bits per pixel, using SkColorTable to specify the colors
-    RGB_565,    //!< 16-bits per pixel, (see SkColorPriv.h for packing)
-    ARGB_4444,  //!< 16-bits per pixel, (see SkColorPriv.h for packing)
-    ARGB_8888,  //!< 32-bits per pixel, (see SkColorPriv.h for packing)
-    RLE_Index8,
-  };
+struct Bitmap
+{
+    enum Config
+    {
+        NoConfig,   //!< Empty default
+        A8,         //!< 8-bits per pixel, with only alpha specified (0 is transparent, 0xFF is opaque)
+        ARGB_8888,  //!< 32-bits per pixel
+    };
 
-  @property ISize size() const {
-    return ISize(this.width, this.height);
-  }
-  @property IRect bounds() const {
-    return IRect(this.size);
-  }
+private:
+    uint _width;
+    uint _height;
+    Config _config;
+    ubyte _flags;
+    ubyte[] _buffer;
 
-  uint width;
-  uint height;
-  Bitmap.Config config;
-  ubyte flags;
-  ColorTable colorTable;
-  ubyte[] _buffer;
+public:
 
-  this(Config config, uint width, uint height) {
-    this.setConfig(config, width, height);
-  }
-
-  void setConfig(Bitmap.Config config, uint width, uint height) {
-    this.width = width;
-    this.height = height;
-    this.config = config;
-    this._buffer.length = width * height * BytesPerPixel(config);
-  }
-
-  const(T)[] getRangeConst(T=Color)(uint xStart, uint xEnd, uint y) const {
-    return (cast(Bitmap)this).getRange!(T)(xStart, xEnd, y);
-  }
-
-  T[] getRange(T=Color)(uint xStart, uint xEnd, uint y) {
-    assert(xEnd - xStart <= this.width,
-           "start:" ~ to!string(xStart) ~ "end: "~ to!string(xEnd) ~ "width:"~to!string(this.width));
-    assert(y <= this.height, to!string(y));
-    size_t yOff = y * this.width;
-    return this.getBuffer!T()[yOff + xStart .. yOff + xEnd];
-  }
-
-  T[] getBuffer(T=Color)() {
-    return cast(T[])this._buffer;
-  }
-
-  const(T)[] getConstBuffer(T=Color)() const {
-    return cast(const(T)[])this._buffer;
-  }
-
-  auto getLine(uint y) {
-    return this.getRange(0u, this.width, y);
-  }
-
-  @property void opaque(bool isOpaque) {
-    if (isOpaque) {
-      flags |= Flags.kImageIsOpaque_Flag;
+    this(Config config, uint width, uint height)
+    {
+        setConfig(config, width, height);
     }
-    else {
-      flags &= ~Flags.kImageIsOpaque_Flag;
+
+    void setConfig(Bitmap.Config config, uint width, uint height)
+    {
+        _width  = width;
+        _height = height;
+        _config = config;
+        _buffer.length = width * height * BytesPerPixel(config);
     }
-  }
 
-  @property bool opaque() const {
-    final switch (this.config) {
-        case Bitmap.Config.NoConfig:
-            return true;
-
-        case Bitmap.Config.A1:
-        case Bitmap.Config.A8:
-        case Bitmap.Config.ARGB_4444:
-        case Bitmap.Config.ARGB_8888:
-            return (this.flags & Flags.kImageIsOpaque_Flag) != 0;
-
-        case Bitmap.Config.Index8:
-        case Bitmap.Config.RLE_Index8: {
-	  // if lockPixels failed, we may not have a ctable ptr
-	  return this.colorTable &&
-	    ((this.colorTable.flags
-	     & ColorTable.kColorsAreOpaque_Flag) != 0);
-	}
-
-        case Bitmap.Config.RGB_565:
-            return true;
+    @property uint width() const
+    {
+        return _width;
     }
+
+    @property uint height() const
+    {
+        return _height;
+    }
+
+    @property ISize size() const
+    {
+        return ISize(_width, _height);
+    }
+
+    @property IRect bounds() const
+    {
+        return IRect(size);
+    }
+
+    @property Config config() const
+    {
+        return _config;
+    }
+
+    const(T)[] getRangeConst(T=Color)(uint xStart, uint xEnd, uint y) const
+    {
+        return (cast(Bitmap*)&this).getRange!(T)(xStart, xEnd, y);
+    }
+
+    T[] getRange(T=Color)(uint xStart, uint xEnd, uint y)
+    {
+        assert(xEnd - xStart <= _width,
+               "start:" ~ to!string(xStart) ~ "end: "~ to!string(xEnd) ~ "width:"~to!string(_width));
+        assert(y <= _height, to!string(y));
+        size_t yOff = y * _width;
+        return getBuffer!T()[yOff + xStart .. yOff + xEnd];
   }
 
-  void eraseColor(Color c) {
-    if (0 == this.width || 0 == this.height
-	|| this.config == Bitmap.Config.NoConfig
-	|| this.config == Bitmap.Config.Index8)
-      return;
+    T[] getBuffer(T=Color)()
+    {
+        return cast(T[])_buffer;
+    }
 
-    assert(this.config == Bitmap.Config.ARGB_8888);
-    this.getBuffer!Color()[] = c;
-    // this.notifyPixelChanged();
-  }
+    const(T)[] getConstBuffer(T=Color)() const
+    {
+        return cast(const(T)[])_buffer;
+    }
 
-  void save(string path) const {
-    auto bpp = 8 * BytesPerPixel(this.config);
-    if (bpp != 0) {
-      synchronized(freeImage) {
-        FIBITMAP* fibmp;
-        // TODO: check if color masks needed
-        if (this.config == Bitmap.Config.ARGB_8888)
-          fibmp = FreeImage_Allocate(width, height, bpp,
-                                          ColorMask!("r"), ColorMask!("g"), ColorMask!("b"));
+    auto getLine(uint y)
+    {
+        return getRange(0u, _width, y);
+    }
+
+    @property void opaque(bool isOpaque)
+    {
+        if (isOpaque)
+        {
+            _flags |= Flags.opaque;
+        }
         else
-          fibmp = FreeImage_Allocate(width, height, bpp);
-        enforce(fibmp, "error while allocating write image");
-        ubyte* deviceBits = FreeImage_GetBits(fibmp);
-        deviceBits[0 .. this._buffer.length] = this._buffer[];
-        // TODO: possible to avoid flipping
-        FreeImage_FlipVertical(fibmp);
-        FreeImage_Save(FREE_IMAGE_FORMAT.PNG, fibmp, toStringz(path));
-
-        FreeImage_Unload(fibmp);
-      }
+        {
+            _flags &= ~Flags.opaque;
+        }
     }
-  }
 
-  static Bitmap load(string path) {
-    Bitmap result;
-    synchronized(freeImage) {
-      auto cpath = std.string.toStringz(path);
-      FREE_IMAGE_FORMAT fmt = FreeImage_GetFileType(cpath);
-      if (fmt == FREE_IMAGE_FORMAT.UNKNOWN)
-        fmt = FreeImage_GetFIFFromFilename(cpath);
-      enforce(fmt != FREE_IMAGE_FORMAT.UNKNOWN && FreeImage_FIFSupportsReading(fmt),
-              "unsupported file " ~ path);
-      FIBITMAP* fibmp = enforce(FreeImage_Load(fmt, cpath), "error while decoding " ~ path);
-      // TODO: possible to avoid flipping
-      FreeImage_FlipVertical(fibmp);
-
-      auto bpp = FreeImage_GetBPP(fibmp);
-      switch (bpp) {
-      case 8:
-        // TODO: check for palettized color images
-        result.config = Bitmap.Config.A8;
-        break;
-
-      case 48: // for now
-      case 64: // for now
-      case 24:
-        auto cpy = FreeImage_ConvertTo32Bits(fibmp);
-        FreeImage_Unload(fibmp);
-        fibmp = cpy;
-        bpp = 32;
-        goto case;
-
-      case 32:
-        result.config = Bitmap.Config.ARGB_8888;
-        break;
-
-      default:
-        assert(0, "unsupported bit depth " ~ std.conv.to!string(bpp));
-      }
-
-      auto w = FreeImage_GetWidth(fibmp);
-      auto h = FreeImage_GetHeight(fibmp);
-      auto nbytes =  (w * h * bpp) >> 3;
-      ubyte* bits = FreeImage_GetBits(fibmp);
-      result._buffer = bits[0 .. nbytes].dup;
-
-      result.width = w;
-      result.height = h;
-
-      FreeImage_Unload(fibmp);
+    @property bool opaque() const
+    {
+        return !!(_flags & Flags.opaque);
     }
-    return result;
-  }
+
+    void eraseColor(Color c)
+    {
+        assert(_config == Bitmap.Config.ARGB_8888);
+        getBuffer!Color()[] = c;
+    }
+
+    void save(string path) const
+    {
+        auto bpp = 8 * BytesPerPixel(_config);
+        if (bpp != 0)
+        {
+            synchronized(freeImage)
+            {
+                FIBITMAP* fibmp;
+                // TODO: check if color masks needed
+                if (_config == Bitmap.Config.ARGB_8888)
+                {
+                    fibmp = FreeImage_Allocate(_width, _height, bpp,
+                                               ColorMask!("r"), ColorMask!("g"), ColorMask!("b"));
+                }
+                else
+                {
+                    fibmp = FreeImage_Allocate(_width, _height, bpp);
+                }
+                enforce(fibmp, "error while allocating write image");
+                ubyte* deviceBits = FreeImage_GetBits(fibmp);
+                deviceBits[0 .. _buffer.length] = _buffer[];
+                // TODO: possible to avoid flipping
+                FreeImage_FlipVertical(fibmp);
+                FreeImage_Save(FREE_IMAGE_FORMAT.PNG, fibmp, toStringz(path));
+
+                FreeImage_Unload(fibmp);
+            }
+        }
+    }
+
+    static Bitmap load(string path)
+    {
+        Bitmap result;
+        synchronized(freeImage)
+        {
+            immutable cpath = toStringz(path);
+            FREE_IMAGE_FORMAT fmt = FreeImage_GetFileType(cpath);
+
+            if (fmt == FREE_IMAGE_FORMAT.UNKNOWN)
+                fmt = FreeImage_GetFIFFromFilename(cpath);
+
+            enforce(fmt != FREE_IMAGE_FORMAT.UNKNOWN && FreeImage_FIFSupportsReading(fmt),
+                    format("Unsupported image format %s.", path));
+
+            FIBITMAP* fibmp = enforce(FreeImage_Load(fmt, cpath),
+                                      format("Error while decoding %s.", path));
+            // TODO: possible to avoid flipping
+            FreeImage_FlipVertical(fibmp);
+
+            Config config;
+            auto bpp = FreeImage_GetBPP(fibmp);
+            switch (bpp)
+            {
+            case 8:
+                // TODO: check for palettized color images
+                config = Config.A8;
+                break;
+
+            case 48: // for now
+            case 64: // for now
+            case 24:
+                auto cpy = FreeImage_ConvertTo32Bits(fibmp);
+                FreeImage_Unload(fibmp);
+                fibmp = cpy;
+                bpp = 32;
+                goto case;
+
+            case 32:
+                config = Config.ARGB_8888;
+                break;
+
+            default:
+                assert(0, format("Unsupported bit depth %s for image %s.", bpp, path));
+            }
+
+            immutable w = FreeImage_GetWidth(fibmp);
+            immutable h = FreeImage_GetHeight(fibmp);
+            result.setConfig(config, w, h);
+            immutable nbytes =  (w * h * bpp) >> 3;
+            ubyte* bits = FreeImage_GetBits(fibmp);
+            result._buffer[] = bits[0 .. nbytes];
+
+            FreeImage_Unload(fibmp);
+        }
+        return result;
+    }
 
 private:
 
-  enum Flags
-  {
-    kImageIsOpaque_Flag = 0x01,
-  }
+    enum Flags
+    {
+        opaque = 1 << 0,
+    }
 }
 
 
-size_t RowBytes(Bitmap.Config c, int width) {
-  assert(width > 0);
-  return c == Bitmap.Config.A1 ? (width + 7) >> 3 : width * BytesPerPixel(c);
-}
-
-uint BytesPerPixel(Bitmap.Config c) {
-  final switch (c) {
-  case Bitmap.Config.NoConfig, Bitmap.Config.A1:
-    return 0;
-  case Bitmap.Config.RLE_Index8, Bitmap.Config.A8, Bitmap.Config.Index8:
-    return 1;
-  case Bitmap.Config.RGB_565, Bitmap.Config.ARGB_4444:
-    return 2;
-  case Bitmap.Config.ARGB_8888:
-    return 4;
-  }
+uint BytesPerPixel(Bitmap.Config c)
+{
+    final switch (c)
+    {
+    case Bitmap.Config.NoConfig:
+        return 0;
+    case Bitmap.Config.A8:
+        return 1;
+    case Bitmap.Config.ARGB_8888:
+        return 4;
+    }
 }
 
 // TODO: move freeimage functions into class so they are protected by a lock
